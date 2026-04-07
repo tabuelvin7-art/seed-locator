b37ddf791637edde3b5aa9c173855069623d654e
tabuelvin7-art <tabuelvin7@gmail.com>
2026-04-07 14:11:39 +0300

    fix api base url to use env variable

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:5000/api' });
+const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api' });
 
 api.interceptors.request.use((config) => {
   if (typeof window !== 'undefined') {
